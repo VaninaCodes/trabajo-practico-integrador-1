2147c43f7b2b54b8fd5dd78dbb382e1bc697e677
VaninaCodes <Vannilaresquin@gmail.com>
2026-09-07 01:02:56 -0300

    feat: se conecto a la base de datos con las configuraciones en database.js y app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import { startDB } from './src/config/database.js';
+
+const app= express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, async() => {
+    await startDB();
+    console.log(`Servidor listo http://localhost:${PORT}`);
+})
