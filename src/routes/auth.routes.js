9187d276f219ec145ba8ac082d845ca60ece6bec
VaninaCodes <Vannilaresquin@gmail.com>
2026-09-08 00:50:52 -0300

    feat(routes): se agregaron las rutas a seguir de la autorizacion

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import { Router} from "express";
+import {login, logout, register, getProfile, updateProfile} from "../controllers/auth.controller";
+import {authMiddleware} from "../middlewares/auth.middleware.js"
+
+export const authRouter = Router();
+
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.get("/profile", getProfile);
+authRouter.put("/profile", updateProfile);
+authRouter.post("/logout", logout);
